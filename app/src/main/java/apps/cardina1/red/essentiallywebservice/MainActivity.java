1d726e36ab40b7d0854e84f05e98cc30402b1c62
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-07-07 20:44:00 +0900

    Add project template

---
+++ app/src/main/java/apps/cardina1/red/essentiallywebservice/MainActivity.java
@@ -0,0 +1,13 @@
+package apps.cardina1.red.essentiallywebservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
