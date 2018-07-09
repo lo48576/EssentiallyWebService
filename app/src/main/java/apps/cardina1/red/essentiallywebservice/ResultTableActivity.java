cff2d375de3ea014d1678f93605217f1e820e7f0
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-07-09 15:29:25 +0900

    Add a new activity `ResultTableActivity` with default boilerplates

---
+++ app/src/main/java/apps/cardina1/red/essentiallywebservice/ResultTableActivity.java
@@ -0,0 +1,13 @@
+package apps.cardina1.red.essentiallywebservice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_table);
+    }
+}
